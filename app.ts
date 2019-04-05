81c33339d0246676bc0d978da67a7af8177894cd
Brian Juul Rasmussen <brian.juul.rasmussen@gmail.com>
2019-04-05 23:49:34 +0200

    reinstated some code..

--- app.ts
+++ app.ts
@@ -5,62 +5,61 @@ import { Matrix } from "./matrix";
 class Program {
     static main() : void {
         var matrix = new Matrix(2,3);
+        
+        console.log("Problem is:");
+        console.log("4x1 + x2 = 9");
+        console.log("x1 - x2 = 1\n");
+
+        matrix.insertRowAtIndex(0, [ 4, 1, 9 ]);
+        matrix.insertRowAtIndex(1, [ 1, -1, 1]);
+
         matrix.renderMatrix();
-        return;
-        // console.log("Problem is:");
-        // console.log("4x1 + x2 = 9");
-        // console.log("x1 - x2 = 1\n");
 
-        // matrix.insertRowAtIndex(0, [ 4, 1, 9 ]);
-        // matrix.insertRowAtIndex(1, [ 1, -1, 1]);
+        console.log(" - add row 0 to 1");
+        matrix.addToRow(0,1, 1);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - add row 0 to 1");
-        // matrix.addToRow(0,1, 1);
+        console.log(" - scale row 0 by 1/5");
+        matrix.scaleRow(0,1/5);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - scale row 0 by 1/5");
-        // matrix.scaleRow(0,1/5);
+        console.log(" - scale row 1 by -1");
+        matrix.scaleRow(1,-1);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - scale row 1 by -1");
-        // matrix.scaleRow(1,-1);
+        console.log(" - add row 1 to 0");
+        matrix.addToRow(1,0, 1);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - add row 1 to 0");
-        // matrix.addToRow(1,0, 1);
+        console.log("Solution is: [" + matrix.getValueAt(0,2) + "," + matrix.getValueAt(1,2) + "] (x1 = 2, x2 = 1)" );
 
-        // matrix.renderMatrix();
+        // inverse row-operations:
+        console.log(" - add row 1 to 0, with -1 (inverse)");
+        matrix.addToRow(1,0, -1);
 
-        // console.log("Solution is: [" + matrix.getValueAt(0,2) + "," + matrix.getValueAt(1,2) + "] (x1 = 2, x2 = 1)" );
+        matrix.renderMatrix();
 
-        // // inverse row-operations:
-        // console.log(" - add row 1 to 0, with -1 (inverse)");
-        // matrix.addToRow(1,0, -1);
+        console.log(" - scale row 1 by -1 (inverse)");
+        matrix.scaleRow(1,-1);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - scale row 1 by -1 (inverse)");
-        // matrix.scaleRow(1,-1);
+        console.log(" - scale row 0 by 5 (inverse)");
+        matrix.scaleRow(0,5);
 
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
-        // console.log(" - scale row 0 by 5 (inverse)");
-        // matrix.scaleRow(0,5);
+        console.log(" - add row 0 to 1 with -1 (inverse)");
+        matrix.addToRow(0,1, -1);
 
-        // matrix.renderMatrix();
-
-        // console.log(" - add row 0 to 1 with -1 (inverse)");
-        // matrix.addToRow(0,1, -1);
-
-        // matrix.renderMatrix();
+        matrix.renderMatrix();
 
 
-        // matrix.swapRows(0, 1);
+        matrix.swapRows(0, 1);
 
         // matrix.renderMatrix();
 
